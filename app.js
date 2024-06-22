3cab62a7501c921fcf48f51046e0d33c7b7c518e
feeldoom <64213424+feeldoom@users.noreply.github.com>
2024-06-22 20:21:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -80,6 +80,9 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200); 
+  }
   next();
 });
 
