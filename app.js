2abc04c85791791f07ccdabce0448a5231a6ea4d
feeldoom <64213424+feeldoom@users.noreply.github.com>
2024-06-22 18:54:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: 'https://tortuga-front.vercel.app',
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
