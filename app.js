2afb2ad73d311b8af258ccc7f2b37f823944f6c5
feeldoom <64213424+feeldoom@users.noreply.github.com>
2024-06-23 11:36:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -99,7 +99,14 @@ app.use(session({
   secret: 'TzDFG8O5cF',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false },
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI,
+    collectionName: 'sessions'
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 // 1 Day
+  }
 }));
 
 app.use(express.urlencoded({ extended: true }));
