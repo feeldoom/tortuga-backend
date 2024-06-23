8b6a31802b1c94e7c83daa8b21cda02e06428779
feeldoom <64213424+feeldoom@users.noreply.github.com>
2024-06-23 17:51:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -257,16 +257,13 @@ app.get('/pdfs', async (req, res) => {
 //   }
 // });
 
-app.post('/uploadPost', requireAuth, uploadPostPhoto.array('photos', 10), async (req, res) => {
+app.post('/uploadPost', requireAuth, uploadPostPhoto.fields([{ name: 'photo', maxCount: 1 }]), async (req, res) => {
   try {
-    const photoFiles = req.files['photos'] || [];
-    const photoUrls = await Promise.all(photoFiles.map(async (photoFile) => {
-      const photoUrl = await uploadFile(photoFile);
-      return photoUrl;
-    }));
+    const photoFile = req.files['photo'] ? req.files['photo'][0] : null;
+    const photoUrl = photoFile ? await uploadFile(photoFile) : null;
 
     const { title, text } = req.body;
-    const newPost = new Post({ title, text, photo: photoUrls });
+    const newPost = new Post({ title, text, photo: photoUrl });
     await newPost.save();
 
     res.redirect(`https://tortuga-front.vercel.app/admin.html?status=success`);
