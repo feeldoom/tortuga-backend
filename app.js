0d24293040730df069d555fcbf6306ad35bdf585
feeldoom <64213424+feeldoom@users.noreply.github.com>
2024-06-23 21:16:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: '*',
+  origin: ["*"],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'access-control-allow-origin']
 }));
